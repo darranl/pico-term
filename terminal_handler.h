e8bbaa17ae0cb71cc748afff2cf318a73feca9dc
Darran A Lofthouse <darran@lofthouse.me.uk>
2025-03-22 16:43:19 +0000

    Increase the buffer size.

--- terminal_handler.h
+++ terminal_handler.h
@@ -2,16 +2,16 @@
  *
  * This file is part of pico-term.
  *
- * pico-term is free software: you can redistribute it and/or modify it under the terms 
- * of the GNU General Public License as published by the Free Software Foundation, either 
+ * pico-term is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
  *
- * pico-term is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * pico-term is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with pico-term.
- * If  not, see <https://www.gnu.org/licenses/>. 
+ * If  not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef TERMINAL_HANDLER_H
@@ -20,7 +20,7 @@
 #include "term/vt102.h"
 
 #define CDC_INTF 0
-#define WRITE_BUFFER_LENGTH 1024
+#define WRITE_BUFFER_LENGTH 2048
 
 typedef void (*vt102_event_handler)(vt102_event *event, void *context);
 
